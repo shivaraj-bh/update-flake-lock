e9f81df8d2fa51b474aeb5083b5c26e621630247
shivaraj-bh <sbh69840@gmail.com>
2024-04-22 21:53:09 +0530

    revamp

--- update-flake-lock.sh
+++ update-flake-lock.sh
@@ -8,24 +8,11 @@ if [[ -n "$NIX_OPTIONS" ]]; then
     done
 fi
 
-if [[ -n "$TARGETS" ]]; then
-    inputs=()
-    for input in $TARGETS; do
-        inputs+=("--update-input" "$input")
+flake_dirs=()
+if [[ -n "$PATH_TO_FLAKE_DIRS" ]]; then
+    for dir in $PATH_TO_FLAKE_DIRS; do
+        flake_dirs+=("--flake" "$dir")
     done
-    if [[ -n "$PATH_TO_FLAKE_DIRS" ]]; then
-        for dir in $PATH_TO_FLAKE_DIRS; do
-            nix "${options[@]}" flake lock "${inputs[@]}" --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG" "$dir"
-        done
-    else
-        nix "${options[@]}" flake lock "${inputs[@]}" --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
-    fi
-else
-    if [[ -n "$PATH_TO_FLAKE_DIRS" ]]; then
-        for dir in $PATH_TO_FLAKE_DIRS; do
-            nix "${options[@]}" flake update --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG" --flake "$dir"
-        done
-    else
-        nix "${options[@]}" flake update --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
-    fi
 fi
+
+nix "${options[@]}" flake update "$TARGETS" --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG" "${flake_dirs[@]}"
